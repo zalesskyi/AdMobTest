c95228efcbd66868ffe3bf606ddb808098dd07c8
alexeyzalesskiy <alexeyzalesskiy08@gmail.com>
2019-03-23 10:13:26 +0200

    Initial commit

---
+++ app/src/main/java/com/zalesskyi/android/admobtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zalesskyi.android.admobtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
